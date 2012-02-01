be27cdee13ec35f47e83bbd1ec9ad27039305f14
Drew Malone <dmalone@tacc.utexas.edu>
2012-02-01 09:37:46 -0600

    Removed deprecated imports.

--- tacc_stats_web/apps/tacc_stats/urls.py
+++ tacc_stats_web/apps/tacc_stats/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import DetailView, ListView
 from tacc_stats.models import Job
-from tacc_stats.views import index, job_memused_hist, job_timespent_hist, job_mem_heatmap, job_files_open_heatmap, create_heatmap
+from tacc_stats.views import index, job_memused_hist, job_timespent_hist, create_heatmap
 
 urlpatterns = patterns('',
     url(r'^$', index),
